781488682897ee2373891ccdc1cedcd0f9d1f498
Steve Kukla <kukla@adobe.com>
2018-12-28 17:59:15 -0500

    Updated set url command to account for hostname

--- commands.sh
+++ commands.sh
@@ -568,9 +568,9 @@ function set-url() {
   ./bin/magento cache:clean config
   sleep 1
   printf "\nSetting hostname to match new URL..."
-  sudo hostnamectl set-hostname ${NEW_URL}
   sudo sed -i "s|${BASE_URL}|${NEW_URL}|g" /etc/hosts
   sudo sed -i "s|${BASE_URL}|${NEW_URL}|g" /etc/hostname
+  sudo hostnamectl set-hostname ${NEW_URL} /dev/null 2>&1
   printf "done.\n\nHostname set to: "
   hostname
   printf "\nWe need to restart your VM to see this hostname change take full effect.\nRebooting now..."
