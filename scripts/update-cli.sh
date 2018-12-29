fe3e98b6d3d9f844bb8c27a2348907c6688e6d1e
Steve Kukla <kukla@adobe.com>
2018-12-29 12:09:21 -0500

    Fixed issue with interactive version of set-url

--- scripts/update-cli.sh
+++ scripts/update-cli.sh
@@ -9,7 +9,7 @@ printf "\nUpdating the VM CLI...\n"
 printf "\nAdding SSH Key..."
 eval $(ssh-agent) > /dev/null 2>&1
 ssh-add ~/.ssh/Magento-Cloud > /dev/null 2>&1
-printf "done.\n"
+printf "done.\n\n"
 
 # Update CLI
 cd ${CLI_DIRECTORY}
