ddda97236a13129f4a09edfecfb3c688f9bec7eb
Steve Kukla <kukla@adobe.com>
2018-12-29 11:27:36 -0500

    Updated update-cli command to add chmod -x

--- scripts/update-cli.sh
+++ scripts/update-cli.sh
@@ -12,16 +12,22 @@ ssh-add ~/.ssh/Magento-Cloud
 
 # Update CLI
 cd ${CLI_DIRECTORY}
+
+# Stash existing changes
+git stash
+
+# Pull new changes and set permissions
 git pull
 printf "\nInstalling commands... "
 source ${CLI_DIRECTORY}/commands.sh
 sleep 1
 printf "done. \n\nMaking scripts executable... "
 sudo chmod +x ${CLI_DIRECTORY}/${SCRIPTS_DIRECTORY}/*.sh
-# Unstage the copied script files
-git checkout -- *
 sleep 1
 printf "done.\n\n"
+
+# Drop stash
+git stash drop
 
 # Move to web root
 cd ${MAGENTO_DIRECTORY}
