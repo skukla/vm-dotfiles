7e1ed10db31274dccfa2ec91e56f35c8391d4986
Steve Kukla <kukla@adobe.com>
2018-12-29 12:46:33 -0500

    Updating url-check script to fix issues

--- commands.sh
+++ commands.sh
@@ -405,6 +405,6 @@ export -f get-url
 function set-url() {
   CLI_DIRECTORY=~/cli
   SCRIPTS_DIRECTORY=scripts
-  bash ${CLI_DIRECTORY}/${SCRIPTS_DIRECTORY}/url-check.sh null foo
+  bash ${CLI_DIRECTORY}/${SCRIPTS_DIRECTORY}/url-check.sh null bar
 }
 export -f set-url
