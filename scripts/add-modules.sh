0bfafe7c1fd6562be339d6327a21ebff40c060f0
Steve Kukla <kukla@adobe.com>
2019-01-08 10:03:51 -0800

    Updated add-modules script

--- scripts/add-modules.sh
+++ scripts/add-modules.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
-source ~/.bashrc
+GROUP=vagrant
+USER=vagrant
+MAGENTO_DIRECTORY=/var/www/magento
 
-www
-own
-disable-cron
-add-key
-update-composer
-db-upgrade
-di-compile
-deploy-content
-clean
-enable-cache
-enable-cron
+cd ${MAGENTO_DIRECTORY} 
+
+printf "Updating permissions..."
+chown -R ${GROUP}:${USER} var/cache/ var/page_cache/
+chmod -R 777 var/ pub/ app/etc/ generated/
+printf "done.\n"
+
+printf "\nAdding SSH keys...\n"
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_rsa.skukla.gitlab
+ssh-add ~/.ssh/id_rsa.skukla.github.magento-cloud
+
+printf "\nRemoving cron...\n"
+./bin/magento cron:remove
+crontab -r
+
+printf "\nDownloading code...\n"
+composer update
+
+printf "\nUpdating the database...\n"
+./bin/magento setup:upgrade
+
+printf "\nCompiling dependencies...\n"
+./bin/magento setup:di:compile
+
+printf "\nDeploying static content..."
+./bin/magento set:static-content:deploy -f
+
+printf "\nReindexing...\n"
+./bin/magento indexer:reindex
+
+exit
