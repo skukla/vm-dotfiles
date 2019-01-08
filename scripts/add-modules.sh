6f338c291df1e8827ea4ad428d888055a5e4542e
Steve Kukla <kukla@adobe.com>
2019-01-08 10:45:22 -0800

    Add sleep statements to add-modules

--- scripts/add-modules.sh
+++ scripts/add-modules.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export TERM=xterm-256color
 GROUP=vagrant
 USER=vagrant
 MAGENTO_DIRECTORY=/var/www/magento
@@ -7,24 +8,25 @@ clear
 
 printf "Beginning the update process...\n"
 
-cd ${MAGENTO_DIRECTORY} 
+cd ${MAGENTO_DIRECTORY}
+sleep 2
 
 printf "\nUpdating permissions..."
 chown -R ${GROUP}:${USER} var/cache/ var/page_cache/
 chmod -R 777 var/ pub/ app/etc/ generated/
 printf "done.\n"
-sleep 1
+sleep 2
 
 printf "\nAdding SSH keys...\n"
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_rsa.skukla.gitlab
 ssh-add ~/.ssh/id_rsa.skukla.github.magento-cloud
-sleep 1
+sleep 2
 
 printf "\nRemoving cron...\n"
 ./bin/magento cron:remove
 crontab -r
-sleep 1
+sleep 2
 
 printf "\nDownloading code...\n"
 composer update
@@ -40,7 +42,7 @@ printf "\nDeploying static content..."
 
 printf "\nReindexing...\n"
 ./bin/magento indexer:reindex
-sleep 1
+sleep 2
 
 printf "\nUpgrade finished!\n"
 
