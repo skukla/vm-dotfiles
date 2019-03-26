9dae12f38693492cc0766ac06b20634ed5c0b4f9
Steve Kukla <kukla@adobe.com>
2019-03-25 23:33:48 -0600

    Added Custom B2B cache warmer

--- scripts/cache-warmer.sh
+++ scripts/cache-warmer.sh
@@ -29,7 +29,7 @@ printf "done.\n"
 clear
 printf "Warming the Custom B2B cache...\n"
 sleep 1
-wget --quiet "http://b2b.luma.com/pub/custom_b2b.xml" --no-cache --output-document - | egrep -o "http://luma.com[^<]+" | while read line; do
+wget --quiet "http://b2b.luma.com/pub/custom_b2b.xml" --no-cache --output-document - | egrep -o "http://b2b.luma.com[^<]+" | while read line; do
     time curl -A 'Cache Warmer' -s -L $line > /dev/null 2>&1
     echo $line
 done
