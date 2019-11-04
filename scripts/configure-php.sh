190ab40a04074283d009a0d9b6bb54bedb698b50
Steve Kukla <kukla@adobe.com>
2019-11-04 18:17:55 -0500

    Converted configure-php to a script

--- scripts/configure-php.sh
+++ scripts/configure-php.sh
@@ -43,7 +43,7 @@ function list_php() {
 
 ### START ###
 clear
-if [[ ! $(inArray 7.0 ${SUPPORTED_VERSIONS[@]}) ]]; then
+if [[ ! $(inArray "7.0" ${SUPPORTED_VERSIONS[@]}) ]]; then
     printf "No match"
 else
     printf "Match"
