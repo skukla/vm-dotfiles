e95d8389fafb9f7258ba0f2e507421f69d5c0a2c
Steve Kukla <kukla@adobe.com>
2019-11-04 23:56:55 -0500

    Updated configure-php script

--- scripts/configure-php.sh
+++ scripts/configure-php.sh
@@ -26,7 +26,7 @@ function check_php() {
     else
         printf "\nHere are the versions of PHP currently available on the system:\n\n"
         ls -la /etc/php
-        return 1
+        exit
     fi
 }
 
